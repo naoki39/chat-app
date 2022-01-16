1f3ee5eddd67095a129c80e23e69ceba2f5a46f9
naoki39 <naokibad0311@gmail.com>
2022-01-16 09:41:11 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'messages/index'
  root to: "messages#index"
- resources :users, only: [:edit, :update,:create]
+ resources :users, only: [:edit, :update,:create,]
+ resources :rooms, only: [:new, :create]
 end
--- db/migrate/20220114232658_devise_create_rooms.rb
+++ db/migrate/20220114232658_devise_create_rooms.rb
@@ -8,8 +8,7 @@ class DeviseCreateRooms < ActiveRecord::Migration[6.0]
       t.timestamps null: false
     end
 
-    add_index :rooms, :email,                unique: true
-    add_index :rooms, :reset_password_token, unique: true
+  
     # add_index :rooms, :confirmation_token,   unique: true
     # add_index :rooms, :unlock_token,         unique: true
   end
