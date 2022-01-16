cf03aa643b537b47a58685c94b40b33d046c30fb
naoki39 <naokibad0311@gmail.com>
2022-01-16 12:25:00 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :rooms
-  devise_for :contorollers
   devise_for :users
-  get 'messages/index'
- root to: "messages#index"
- resources :users, only: [:edit, :update,:create,]
- resources :rooms, only: [:new, :create]
+  root to: "rooms#index"
+  resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
